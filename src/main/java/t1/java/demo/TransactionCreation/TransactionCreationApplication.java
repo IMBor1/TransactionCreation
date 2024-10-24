b267179c82e9b2e5789886d056aa5399d07a8352
IMBor1 <ilacerber1@gmail.com>
2024-10-24 23:11:59 +0300

    init

---
+++ src/main/java/t1/java/demo/TransactionCreation/TransactionCreationApplication.java
@@ -0,0 +1,13 @@
+package t1.java.demo.TransactionCreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionCreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionCreationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/t1/java/demo/TransactionCreation/TransactionCreationApplicationTests.java
@@ -0,0 +1,13 @@
+package t1.java.demo.TransactionCreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionCreationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
